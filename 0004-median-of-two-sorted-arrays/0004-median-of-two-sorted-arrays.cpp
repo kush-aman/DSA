de43600d72403f7aebb81e99b01a00b3f8db9fea
Aman kushwaha <007rkush@gmail.com>
2026-06-30 22:53:50 +0530

    Time: 0 ms (100%), Space: 95.2 MB (42.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> arr;
-        int i = 0;
-        int j = 0;
-        while(i < nums1.size() && j < nums2.size()){
-            if(nums1[i] > nums2[j]){
-                arr.push_back(nums2[j]);
-                j++;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int n = n1 + n2;
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;
+        while(low <= high){
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return max(l1,l2);
+                else return (double)((max(l1,l2) + min(r1,r2)) / 2.0);
             }
-            else{
-                arr.push_back(nums1[i]);
-                i++;
-            }
+            else if(l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-        while(i < nums1.size()){
-            arr.push_back(nums1[i]);
-            i++;
-        }
-        while(j < nums2.size()){
-            arr.push_back(nums2[j]);
-            j++;
-        }
-
-        int low = 0;
-        int high = arr.size() - 1;
-        int mid = (low + high) / 2;
-        if(mid - low != high - mid){
-            return (double)(arr[mid] + arr[mid+1])/2;
-        }
-        return arr[mid];
+        return 0;
     }
 };
