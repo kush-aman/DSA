ddc6bbd75ea787b8fa3956476b9147b8c24f7ff7
Aman kushwaha <007rkush@gmail.com>
2026-07-01 14:07:14 +0530

    Time: 0 ms (100%), Space: 64.8 MB (40.85%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int lowerBound(vector<int> arr, int x){
-        int low = 0;
-        int high = arr.size() - 1;
-        int ans;
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(arr[mid] >= x){
-                ans = mid;
-                high = mid - 1;
-            }
-            else low = mid + 1;
-        }
-        return ans;
-    }
 vector<int> rowAndMaximumOnes(vector<vector<int>> &mat) {
     int n = mat.size();
     int m = mat[0].size();
-    for(int i = 0; i < n;i++){
-        sort(mat[i].begin(),mat[i].end());
-    }
-        int cntMax = 0;
-        int index = 0;
-        for(int i = 0;i < n;i++){
-            int cntOnes = m - lowerBound(mat[i],1);
-            if(cntOnes > cntMax){
-                cntMax = cntOnes;
-                index = i;
+    int cntMax = 0;
+    int index = 0;
+    for(int i = 0;i < n;i++){
+        int cntOnes = 0;
+        for(int j = 0; j < m;j++){
+            if(mat[i][j] == 1){
+                cntOnes++;
             }
         }
-    return {index,cntMax};
+        if(cntOnes > cntMax){
+            cntMax = cntOnes;
+            index = i;
+        }
     }
+    return {index,cntMax};
+}
 };
