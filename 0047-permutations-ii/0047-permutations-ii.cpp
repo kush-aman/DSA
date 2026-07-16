46377b344c7f5263e8a812e74bc00a0b7cdb9806
Aman kushwaha <007rkush@gmail.com>
2026-07-16 13:35:33 +0530

    Time: 139 ms (5%), Space: 43.9 MB (5%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    void func(set<vector<int>>& ans, vector<int>& ds, vector<int> nums, int freq[]){
-        if(ds.size() == nums.size()){
-            ans.insert(ds);
+    void func(int index, set<vector<int>>& ans, vector<int> nums){
+        if(index == nums.size()){
+            ans.insert(nums);
             return;
         }
 
-        for(int i = 0; i < nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                func(ans, ds, nums, freq);
-                freq[i] = 0;
-                ds.pop_back();
-            }
+        for(int i = index; i < nums.size();i++){
+            swap(nums[index],nums[i]);
+            func(index + 1, ans, nums);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         set<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        for(int i =0; i< nums.size();i++) freq[i] = 0;
-
-        func(ans, ds, nums, freq);
-
-        vector<vector<int>> finalAns(ans.begin(),ans.end());
-        return finalAns;
+        func(0, ans, nums);
+        vector<vector<int>> final(ans.begin(),ans.end());
+        return final;
     }
 };
