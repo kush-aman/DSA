5004539f68bbdc61752a4a2f104b562e267d6bbb
Aman kushwaha <007rkush@gmail.com>
2026-08-01 13:34:22 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.59%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -7,22 +7,11 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * 
  */
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        int sum = 0;
-        if(root -> left){
-            TreeNode* node = root -> left;
-            sum += node -> val;
-        }
-        if(root -> right){
-            TreeNode* node = root -> right;
-            sum += node -> val;
-        }
-
-        if(sum == root -> val) return true;
-        return false;
+        return root -> val == (root -> left -> val + root -> right -> val);
     }
 };
