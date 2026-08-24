e25d7a555c2f439e2e3e5452c2be53f0dc2f894e
Aman kushwaha <007rkush@gmail.com>
2026-08-24 18:58:16 +0530

    Time: 0 ms (100%), Space: 37.1 MB (95.4%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,23 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ class BSTiterator{
+    stack<TreeNode*> st;
+    bool reverse = true;
+public:
+    BSTiterator(TreeNode* root, bool isReverse){
+        reverse = isReverse;
+        pushAll(root);
+    }
+    bool hasNext(){
+        return !st.empty();
+    }
+    int next(){
+        TreeNode* temp = st.top();
+        st.pop();
+        if(!reverse) pushAll(temp -> right);
+        else pushAll(temp -> left);
+        return temp -> val;
+    }
+private:
+    void pushAll(TreeNode* node){
+        for(; node != NULL; ){
+            st.push(node);
+            if(reverse == true){
+                node = node -> right;
+            }
+            else node = node -> left;
+        }
+    }
+ };
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        map<int,TreeNode*> mpp;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            auto p = q.front();
-            q.pop();
-            if(p -> left) q.push(p -> left);
-            if(p -> right) q.push(p -> right);
-            TreeNode* temp = p;
-            int more = k - (temp -> val);
-            if(mpp.find(more) != mpp.end()){
-                return true;
-            }
-            mpp[temp -> val] = temp;
+        if(!root) return false;
+        BSTiterator l(root,false);
+        BSTiterator r(root,true);
+
+        int left = l.next();
+        int right = r.next();
+        while(left < right){
+            if(left + right == k) return true;
+            else if(left + right < k) left = l.next();
+            else right = r.next();
         }
         return false;
     }
